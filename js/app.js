812db440594a999c4812e21fcaebd6dde655998f
krevakrevakreva87-hash <krevakrevakreva87@gmail.com>
2026-07-23 14:14:29 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,28 +1,31 @@
 const rooms = [
-  const modal = document.getElementById("reservationModal");
-const roomTitle = document.getElementById("roomTitle");
-const closeBtn = document.getElementById("closeBtn");
   "201","202",
   "301","302","303","305","306","308","310",
   "401","402","403"
 ];
 
 const roomsDiv = document.getElementById("rooms");
+const modal = document.getElementById("reservationModal");
+const roomTitle = document.getElementById("roomTitle");
+const closeBtn = document.getElementById("closeBtn");
 
 rooms.forEach(room => {
-  closeBtn.onclick = () => {
-    modal.style.display = "none";
-};
   const card = document.createElement("div");
   card.className = "room-card";
 
-card.onclick = () => {
-    alert(room + "号室を選択しました");
-};
+  card.onclick = () => {
+    roomTitle.textContent = room + "号室の予約";
+    modal.style.display = "block";
+  };
 
-card.innerHTML = `
+  card.innerHTML = `
     <h3>${room}号室</h3>
     <p>🟢 空室</p>
-`;
-roomsDiv.appendChild(card);
+  `;
+
+  roomsDiv.appendChild(card);
 });
+
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+};
